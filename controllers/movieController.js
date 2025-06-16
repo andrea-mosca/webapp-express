19941eb65c428aa16f75bbe4d4016344ab06ed93
andrea-mosca <andrea.moscateli@gmail.com>
2025-06-16 17:26:33 +0200

    add review to SHOW

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -11,15 +11,34 @@ const index = (req, res) => {
 };
 
 const show = (req, res) => {
-  const movieSql = "SELECT * FROM movies.movies WHERE id=?";
   const { id } = req.params;
 
-  connection.query(movieSql, [id], (err, results) => {
+  const movieSql = `SELECT * FROM movies.movies WHERE id=?`;
+  const reviewsSql = `
+    SELECT 
+      movies.*, 
+      reviews.name AS reviewed_name,
+      reviews.vote AS review_vote,
+      reviews.text AS review_text,
+      reviews.created_at,
+      reviews.updated_at
+    FROM movies.movies
+    INNER JOIN movies.reviews ON movies.id = reviews.movie_id
+    WHERE movies.id = ?
+  `;
+
+  connection.query(movieSql, [id], (err, moviesResults) => {
     if (err) return res.status(500).json({ error: "Internal server error" });
-    if (!results.length)
+    if (moviesResults.length === 0)
       return res.status(404).json({ error: "Resource not found" });
-    res.json({
-      movie: results[0],
+
+    const movie = moviesResults[0];
+
+    connection.query(reviewsSql, [id], (err, reviewsResults) => {
+      if (err) return res.status(500).json({ error: "Database query failed" });
+
+      movie.reviews = reviewsResults; // meglio usare 'reviews' al plurale
+      res.json(movie);
     });
   });
 };
