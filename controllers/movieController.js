8191f4b6faadaf3a19827ccb9ea0144c35bef4ae
andrea-mosca <andrea.moscateli@gmail.com>
2025-06-16 16:05:02 +0200

    fix route show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,11 +1,27 @@
 const connection = require("../db/conn.js");
+
 const index = (req, res) => {
-  connection.query("SELECT * FROM movies.movies", (err, results) => {
-    if (err) throw err;
+  const movieSql = "SELECT * FROM movies.movies";
+  connection.query(movieSql, (err, results) => {
+    if (err) return res.status(500).json({ error: "Internal server error" });
     res.json({
       movies: results,
     });
   });
 };
 
-module.exports = { index };
+const show = (req, res) => {
+  const movieSql = "SELECT * FROM movies.movies WHERE id=?";
+  const { id } = req.params;
+
+  connection.query(movieSql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: "Internal server error" });
+    if (!results.length)
+      return res.status(404).json({ error: "Resource not found" });
+    res.json({
+      movie: results[0],
+    });
+  });
+};
+
+module.exports = { index, show };
--- routers/movieRouters.js
+++ routers/movieRouters.js
@@ -2,7 +2,8 @@ const express = require(`express`);
 const router = express.Router();
 const connection = require("../db/conn.js");
 
-const { index } = require("../controllers/movieController.js");
-router.get(`/`, index);
+const { index, show } = require("../controllers/movieController.js");
+router.get("/", index);
+router.get("/:id", show);
 
 module.exports = router;
